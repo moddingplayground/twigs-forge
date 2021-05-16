eb67fd83bb0f6ae8aa6e7a5df2ba47268ad58ea0
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2021-05-16 18:59:06 +0200

    Added twigs item group and chiseled bricks, webhook testing

--- src/main/java/com/ninni/twigs/init/TwigsBlocks.java
+++ src/main/java/com/ninni/twigs/init/TwigsBlocks.java
@@ -1,4 +1,30 @@
 package com.ninni.twigs.init;
 
+
+import com.ninni.twigs.Twigs;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+@SuppressWarnings("unused")
 public class TwigsBlocks {
+    public static final Block CHISELED_BRICKS = register("chiseled_bricks", new Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES).strength(2.0F, 6.0F)));
+
+
+    private static Block register(String id, Block block, boolean registerItem) {
+        Block registered = Registry.register(Registry.BLOCK, new Identifier(Twigs.MOD_ID, id), block);
+        if (registerItem) {
+            Registry.register(Registry.ITEM, new Identifier(Twigs.MOD_ID, id), new BlockItem(registered, new FabricItemSettings().group(Twigs.ITEM_GROUP)));
+        }
+        return registered;
+    }
+    private static Block register(String id, Block block) {
+        return register(id, block, true);
+    }
 }
