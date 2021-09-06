3f24655981f37377c55a09debc52098ae68fc40c
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2021-09-06 03:25:41 +0200

    Added generation for rhyolite and remade many block textures

--- src/main/java/com/ninni/twigs/init/TwigsBiomes.java
+++ src/main/java/com/ninni/twigs/init/TwigsBiomes.java
@@ -11,5 +11,6 @@ public class TwigsBiomes {
     static {
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.MOUNTAINS, BiomeKeys.WOODED_MOUNTAINS, BiomeKeys.GRAVELLY_MOUNTAINS, BiomeKeys.MODIFIED_GRAVELLY_MOUNTAINS, BiomeKeys.MOUNTAIN_EDGE), GenerationStep.Feature.UNDERGROUND_ORES, BuiltInRegistryKeys.get(TwigsConfiguredFeatures.ORE_SCHIST));
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.NETHER_WASTES, BiomeKeys.WARPED_FOREST, BiomeKeys.CRIMSON_FOREST, BiomeKeys.SOUL_SAND_VALLEY), GenerationStep.Feature.UNDERGROUND_ORES, BuiltInRegistryKeys.get(TwigsConfiguredFeatures.ORE_BLOODSTONE));
+        BiomeModifications.addFeature(BiomeSelectors.all(), GenerationStep.Feature.UNDERGROUND_ORES, BuiltInRegistryKeys.get(TwigsConfiguredFeatures.ORE_RHYOLITE));
     }
 }
--- src/main/java/com/ninni/twigs/init/TwigsConfiguredFeatures.java
+++ src/main/java/com/ninni/twigs/init/TwigsConfiguredFeatures.java
@@ -9,8 +9,9 @@ import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.*;
 
 public class TwigsConfiguredFeatures {
-    public static final ConfiguredFeature<?, ?> ORE_SCHIST = register("ore_schist", Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, States.SCHIST, 50)).uniformRange(YOffset.fixed(0), YOffset.fixed(79)).spreadHorizontally().repeat(20));
-    public static final ConfiguredFeature<?, ?> ORE_BLOODSTONE = register("ore_bloodstone", Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.NETHERRACK, States.BLOODSTONE, 33)).uniformRange(YOffset.fixed(0), YOffset.fixed(79)).spreadHorizontally().repeat(5));
+    public static final ConfiguredFeature<?, ?> ORE_SCHIST = register("ore_schist", Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, States.SCHIST, 45)).uniformRange(YOffset.fixed(0), YOffset.fixed(79)).spreadHorizontally().repeat(10));
+    public static final ConfiguredFeature<?, ?> ORE_BLOODSTONE = register("ore_bloodstone", Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.NETHERRACK, States.BLOODSTONE, 33)).uniformRange(YOffset.fixed(0), YOffset.fixed(79)).spreadHorizontally().repeat(25));
+    public static final ConfiguredFeature<?, ?> ORE_RHYOLITE = register("ore_tuff", ((Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, States.RHYOLITE, 33)).uniformRange(YOffset.fixed(0), YOffset.fixed(16))).spreadHorizontally()).repeat(1));
 
     private static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String id, ConfiguredFeature<FC, ?> configuredFeature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Twigs.MOD_ID, id), configuredFeature);
@@ -18,6 +19,7 @@ public class TwigsConfiguredFeatures {
 
     private static class States {
         public static final BlockState SCHIST = TwigsBlocks.SCHIST.getDefaultState();
+        public static final BlockState RHYOLITE = TwigsBlocks.RHYOLITE.getDefaultState();
         public static final BlockState BLOODSTONE = TwigsBlocks.BLOODSTONE.getDefaultState();
     }
 }
