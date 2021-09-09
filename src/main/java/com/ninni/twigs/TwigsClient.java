c7a1847a63a3019b29cb6f0db52393731dd9c388
Daniel Orr <orrdaniel73@gmail.com>
2021-09-09 17:55:11 +0100

    fix render layers

--- src/main/java/com/ninni/twigs/TwigsClient.java
+++ src/main/java/com/ninni/twigs/TwigsClient.java
@@ -9,22 +9,26 @@ public class TwigsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getTranslucent(),
-                TwigsBlocks.OAK_TABLE,
-                TwigsBlocks.SPRUCE_TABLE,
-                TwigsBlocks.BIRCH_TABLE,
-                TwigsBlocks.ACACIA_TABLE,
-                TwigsBlocks.JUNGLE_TABLE,
-                TwigsBlocks.DARK_OAK_TABLE,
-                TwigsBlocks.CRIMSON_TABLE,
-                TwigsBlocks.WARPED_TABLE,
-                TwigsBlocks.STRIPPED_BAMBOO_TABLE,
-                TwigsBlocks.BAMBOO_LEAVES,
-                TwigsBlocks.PAPER_LANTERN,
-                TwigsBlocks.ALLIUM_PAPER_LANTERN,
-                TwigsBlocks.BLUE_ORCHID_PAPER_LANTERN,
-                TwigsBlocks.DANDELION_PAPER_LANTERN,
-                TwigsBlocks.CRIMSON_ROOTS_PAPER_LANTERN
+        BlockRenderLayerMap.INSTANCE.putBlocks(
+            RenderLayer.getCutout(),
+
+            TwigsBlocks.OAK_TABLE,
+            TwigsBlocks.SPRUCE_TABLE,
+            TwigsBlocks.BIRCH_TABLE,
+            TwigsBlocks.ACACIA_TABLE,
+            TwigsBlocks.JUNGLE_TABLE,
+            TwigsBlocks.DARK_OAK_TABLE,
+            TwigsBlocks.CRIMSON_TABLE,
+            TwigsBlocks.WARPED_TABLE,
+            TwigsBlocks.STRIPPED_BAMBOO_TABLE,
+
+            TwigsBlocks.PAPER_LANTERN,
+            TwigsBlocks.ALLIUM_PAPER_LANTERN,
+            TwigsBlocks.BLUE_ORCHID_PAPER_LANTERN,
+            TwigsBlocks.DANDELION_PAPER_LANTERN,
+            TwigsBlocks.CRIMSON_ROOTS_PAPER_LANTERN,
+
+            TwigsBlocks.BAMBOO_LEAVES
         );
     }
 }
