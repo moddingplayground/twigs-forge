c736b3dd6bdb49bdc338e06bbc3c31cdcf43210b
0rc1nus <mitchellctlui@gmail.com>
2022-02-02 20:00:12 +0800

    Lightning Redox ⚡⚡⚡

--- src/main/java/com/ninni/twigs/block/PillarOxidizableBlock.java
+++ src/main/java/com/ninni/twigs/block/PillarOxidizableBlock.java
@@ -1,5 +1,6 @@
 package com.ninni.twigs.block;
 
+import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
@@ -15,19 +16,19 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Optional;
 import java.util.Random;
-import java.util.function.Supplier;
 
 @SuppressWarnings("deprecation")
 public class PillarOxidizableBlock extends RotatedPillarBlock implements WeatheringCopper {
     private final WeatherState level;
-    static Supplier<BiMap<Block, Block>> NEXT_BY_BLOCK = Suppliers.memoize(() -> {
-        return ImmutableBiMap.<Block, Block>builder().put(TwigsBlocks.COPPER_PILLAR.get(), TwigsBlocks.EXPOSED_COPPER_PILLAR.get()).put(TwigsBlocks.EXPOSED_COPPER_PILLAR.get(), TwigsBlocks.WEATHERED_COPPER_PILLAR.get()).put(TwigsBlocks.WEATHERED_COPPER_PILLAR.get(), TwigsBlocks.OXIDIZED_COPPER_PILLAR.get()).build();
+    public static final Supplier<BiMap<Block, Block>> NEXT_BY_BLOCK = Suppliers.memoize(() -> {
+        return ImmutableBiMap.<Block, Block>builder()
+                .put(TwigsBlocks.COPPER_PILLAR.get(), TwigsBlocks.EXPOSED_COPPER_PILLAR.get())
+                .put(TwigsBlocks.EXPOSED_COPPER_PILLAR.get(), TwigsBlocks.WEATHERED_COPPER_PILLAR.get())
+                .put(TwigsBlocks.WEATHERED_COPPER_PILLAR.get(), TwigsBlocks.OXIDIZED_COPPER_PILLAR.get())
+                .build();
     });
-    static Supplier<BiMap<Block, Block>> PREVIOUS_BY_BLOCK = Suppliers.memoize(() -> {
+    public static final Supplier<BiMap<Block, Block>> PREVIOUS_BY_BLOCK = Suppliers.memoize(() -> {
         return NEXT_BY_BLOCK.get().inverse();
-    });
-    Supplier<BiMap<Block, Block>> WAXABLES = Suppliers.memoize(() -> {
-        return ImmutableBiMap.<Block, Block>builder().put(TwigsBlocks.COPPER_PILLAR.get(), TwigsBlocks.WAXED_COPPER_PILLAR.get()).put(TwigsBlocks.EXPOSED_COPPER_PILLAR.get(), TwigsBlocks.WAXED_EXPOSED_COPPER_PILLAR.get()).put(TwigsBlocks.WEATHERED_COPPER_PILLAR.get(), TwigsBlocks.WAXED_WEATHERED_COPPER_PILLAR.get()).put(TwigsBlocks.OXIDIZED_COPPER_PILLAR.get(), TwigsBlocks.WAXED_OXIDIZED_COPPER_PILLAR.get()).build();
     });
 
     public PillarOxidizableBlock(WeatheringCopper.WeatherState level, BlockBehaviour.Properties settings) {
@@ -50,24 +51,8 @@ public class PillarOxidizableBlock extends RotatedPillarBlock implements Weather
         return Optional.ofNullable(NEXT_BY_BLOCK.get().get(state.getBlock())).map((block) -> block.withPropertiesOf(state));
     }
 
-    public Optional<BlockState> getPreviousState(BlockState state) {
+    public static Optional<BlockState> getPreviousState(BlockState state) {
         return Optional.ofNullable(PREVIOUS_BY_BLOCK.get().get(state.getBlock())).map((block) -> block.withPropertiesOf(state));
-    }
-
-    public static Optional<Block> getPrevious(Block p_154891_) {
-        return Optional.ofNullable(PREVIOUS_BY_BLOCK.get().get(p_154891_));
-    }
-
-    public static Optional<BlockState> getPrevious(BlockState state) {
-        return getPrevious(state.getBlock()).map((map) -> {
-            return map.withPropertiesOf(state);
-        });
-    }
-
-    public Optional<BlockState> getWaxed(BlockState state) {
-        return Optional.ofNullable(WAXABLES.get().get(state.getBlock())).map((blockState) -> {
-            return blockState.withPropertiesOf(state);
-        });
     }
 
     public WeatherState getAge() {
